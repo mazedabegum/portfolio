1486968e838ab81ea392c6588a7510e0a7e16c69
mazedabegum <mazedabegum523@gmail.com>
2024-03-12 15:06:57 +0600

    portfolio

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,40 @@
+function portfolio(){
+    fetch("portfolio.json")
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
+
+
+portfolio();
+
+const displayportfolio = (data) =>{
+
+const colportfolio = document.getElementById("portfolio");
+// console.log(data);
+
+for(const portfolio of data){
+    const div = document.createElement("div");
+    div.classList.add(portfolio)
+    div.innerHTML = `
+    <div class="">
+    <img src="https://ibb.co/Fq28CkP">
+
+    </div> 
+    <h1>${portfolio,Name}</h1>
+    `;
+
+colportfolio.appendChild(div);
+console.log(portfolio);
+
+
+};
+
+}
+
+
+
+
+//---------contact area start---------------
 const form = document.querySelector("form");
 const Name = document.querySelector("#exampleFormControlInput1");
 const Email = document.querySelector("#EexampleFormControlInput1");
@@ -26,4 +63,4 @@ const forminfo = {
 console.log(forminfo);
 }
 
-
+//---------contact area end-----------------------
